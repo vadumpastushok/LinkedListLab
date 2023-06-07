3f95f890171f69d9a2571600fd920859cf9f8353
vadum <vadum.pastushok@gmail.com>
2023-06-07 15:11:49 +0300

    changed logic to java List

--- src/main/java/org/example/CircularLinkedList.java
+++ src/main/java/org/example/CircularLinkedList.java
@@ -1,213 +1,62 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CircularLinkedList<T> {
 
-    private Node<T> head;
+    private final List<T> myList = new ArrayList<>();
 
     public Integer length() {
-        if (head == null) {
-            return 0;
-        }
-
-        Node<T> node = head;
-        int length = 1;
-        while (node != null && node.getNext() != head) {
-            length++;
-            node = node.getNext();
-        }
-        return length;
+        return myList.size();
     }
 
     public void append(T element) {
-        if (head == null) {
-            head = new Node<T>(element, null);
-            head.updateNext(head);
-            return;
-        }
-
-        Node<T> node = head;
-        while (node.getNext() != head) {
-            node = node.getNext();
-        }
-
-        Node<T> newNode = new Node<>(element, head);
-        node.updateNext(newNode);
+        myList.add(element);
     }
 
     public void insert(T element, Integer index) {
-        if (index < 0 || index > length()) {
-            throw new IndexOutOfBoundsException("incorrect index");
-        }
-        if (head == null) {
-            head = new Node<T>(element, null);
-            head.updateNext(head);
-            return;
-        }
-
-        Node<T> node = head;
-        int i = 0;
-        while (i != index - 1 && node.getNext() != head) {
-            i++;
-            node = node.getNext();
-        }
-
-        if (i == index - 1) {
-            Node<T> newNode = new Node<>(element, node.getNext());
-            node.updateNext(newNode);
-        } else if (index == 0) {
-            head = new Node<>(element, head);
-            node.updateNext(head);
-        }
+        myList.add(index, element);
     }
 
-    public void delete(Integer index) {
-        if (head == null) {
-            return;
-        }
-        int i = 0;
-        Node<T> node = head;
-        while (node != null && node.getNext() != head && i != index - 1) {
-            i++;
-            node = node.getNext();
-        }
-
-        if (i == index - 1 && node != null && node.getNext() != head) {
-            node.updateNext(node.getNext().getNext());
-        } else  if (index == 0 && head.getNext() == head) {
-            head = null;
-        } else if (index == 0 && node != null) {
-            node.updateNext(head.getNext());
-            head = head.getNext();
-        }
+    public void delete(int index) {
+        myList.remove(index);
     }
 
     public void deleteAll(T element) {
-        if (head == null) {
-            return;
-        }
-        if (head == element && head.getNext() == head) {
-            head = null;
-            return;
-        }
-        Node<T> node = head;
-        do {
-            if (node.getNext().getData() == element) {
-                if (node.getNext() == head) {
-                    head = node.getNext().getNext();
-                }
-                node.updateNext(node.getNext().getNext());
-            }
-            node = node.getNext();
-        } while (node != head);
+        myList.removeIf(it -> it == element);
     }
 
-    public T get(Integer index) {
-        int i = 0;
-        Node<T> node = head;
-        while (node != null && node.getNext() != head && i < index) {
-            i++;
-            node = node.getNext();
-        }
-        if (node == null || i != index) {
-            throw new IndexOutOfBoundsException("incorrect index");
-        } else {
-            return node.getData();
-        }
+    public T get(int index) {
+        return myList.get(index);
     }
 
     public CircularLinkedList<T> cloneList() {
-        CircularLinkedList<T> list = new CircularLinkedList<>();
-        if (head == null) {
-            return list;
-        }
-        Node<T> node = head;
-        do {
-            list.append(node.getData());
-            node = node.getNext();
-        } while (node != head);
-        return list;
+        CircularLinkedList<T> newList = new CircularLinkedList<>();
+        myList.forEach(newList::append);
+        return newList;
     }
 
     public void reverse() {
-        if (head == null || head.getNext() == head) {
-            return;
-        }
-
-        Node<T> tail = head;
-
-        Node<T> prevNode = head;
-        Node<T> node = head.getNext();
-        while (node.getNext() != head) {
-            Node<T> nextNode = node.getNext();
-            node.updateNext(prevNode);
-            prevNode = node;
-            node = nextNode;
-        }
-        node.updateNext(prevNode);
-        head = node;
-        tail.updateNext(head);
+        Collections.reverse(myList);
     }
 
     public Integer findFirst(T element) {
-        if (head == null) {
-            return -1;
-        }
-        Node<T> node = head;
-        int result = -1;
-        int index = 0;
-        do {
-            if (node.getData().equals(element)) {
-                result = index;
-                break;
-            }
-            index++;
-            node = node.getNext();
-        } while (node != head);
-        return result;
+        return myList.indexOf(element);
     }
 
     public Integer findLast(T element) {
-        if (head == null) {
-            return -1;
-        }
-        Node<T> node = head;
-        int result = -1;
-        int index = 0;
-        do {
-            if (node.getData().equals(element)) {
-                result = index;
-            }
-            index++;
-            node = node.getNext();
-        } while (node != head);
-        return result;
+        return myList.lastIndexOf(element);
     }
 
     public void clear() {
-        head = null;
+        myList.clear();
     }
 
     public void extend(CircularLinkedList<T> list) {
-        if (list.length() == 0) {
-            return;
+        for (int i = 0;i < list.length(); i++) {
+            myList.add(list.get(i));
         }
-        Node<T> node = head;
-        while (node != null && node.getNext() != head) {
-            node = node.getNext();
-        }
-
-        if (node == null) {
-            node = new Node<>(list.get(0), null);
-            head = node;
-        } else {
-            node.updateNext(new Node<>(list.get(0),  null));
-            node = node.getNext();
-        }
-
-        for (int i = 1; i < list.length(); i++) {
-            node.updateNext(new Node<>(list.get(i), null));
-            node = node.getNext();
-        }
-        node.updateNext(head);
     }
 }
